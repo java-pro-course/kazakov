7e688eb5205ecc926ab4fe9769af7f867184e29d
chewedmole <chewedmole.git@gmail.com>
2022-10-06 19:11:21 +0700

    Казаков_Роман->"Секретный код"<-java-pro

---
+++ hw1/task2.java
@@ -0,0 +1,22 @@
+package ;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int secretCode;
+        do {
+            Scanner in = new Scanner(System.in);
+            secretCode = in.nextInt();
+            if (secretCode == 123) {
+                System.out.println("Correct number!");
+            } else {
+                System.out.println("Incorrect number!");
+            }
+
+        } while (secretCode != 123);
+
+    }
+}
