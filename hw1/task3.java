e4c28d770a640e23959439d2ae363c633136a50f
chewedmole <chewedmole.git@gmail.com>
2022-10-06 19:11:56 +0700

    Казаков_Роман->"От 0 до 10 по while"<-java-pro

---
+++ hw1/task3.java
@@ -0,0 +1,15 @@
+package ;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
